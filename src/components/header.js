54053ae552e3d65b2bd3bb5043dfe8745bc9fe81
Yesu_Vasupalli <yesu191125@gmail.com>
2025-07-23 13:59:18 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Header extends Component {
     render() {
-        return <h1>Welcome to the Counter App! <br/><br/>Simple Counter App 🧮</h1>
+        return <h1>Welcome to the Counter App! <br/><br/>Simple Counter App </h1>
 
     }
 }
